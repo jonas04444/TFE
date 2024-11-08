0298b301d53cf5d66caf15b8441db3ee510842ac
jonas04444 <jonathan04444@gmail.com>
2024-11-08 12:52:57 +0100

    menu pricipal en cours

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,9 +1,16 @@
 from tkinter import *
+graph_select = Tk()
 
 root = Tk()
 
 root.title("analyse temps de parcours")
 root.geometry("600x600")
+root.iconbitmap("logo-tec.ico")
+
+title = Label(root, text="analyse de temps de parcours", font=("Arial", 25))
+title.pack()
+
+button_connect = Button(root, text="connexion", command=graph_select)
+button_connect.pack()
 
 root.mainloop()
-
