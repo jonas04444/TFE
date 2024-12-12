0dde8ea464ca606b854655606018374caf507aa0
jonat <jonathan04444@gmail.com>
2024-12-12 12:02:39 +0100

    ocr ok

--- ocr.py
+++ ocr.py
@@ -1,8 +1,6 @@
 import pytesseract
 from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
 image = Image.open("Sans titre.png")
 text = pytesseract.image_to_string(image)
 print(text)
