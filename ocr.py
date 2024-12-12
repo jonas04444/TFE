1a52c6baa0401edd46f974fbd4a4052c04dd3119
jonat <jonathan04444@gmail.com>
2024-12-12 14:15:14 +0100

    mise en tableau des infos récupérer par l'ocr

--- ocr.py
+++ ocr.py
@@ -1,6 +1,17 @@
 import pytesseract
 from PIL import Image
+import pandas as pd
 
 image = Image.open("Sans titre.png")
 text = pytesseract.image_to_string(image)
-print(text)
+
+#print(text)
+
+lines = text.strip().split("\n")
+
+premierPL = lines[14].strip().split()
+print(premierPL)
+
+for line in lines:
+    ligne_separee = line.strip().split()
+    print(ligne_separee)
